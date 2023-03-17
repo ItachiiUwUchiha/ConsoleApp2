6447697b0925d4b2288b41629784599f6395810d
konradgd2005@gmail.com <konradgd2005@gmail.com>
2023-03-17 12:19:38 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿//popros uzytkownika o podanie imienia
+Console.WriteLine("Podaj swoje imie");
+//deklarujemy nowa zmienna typu tekstowego o nazwie "name
+string name = "";
+//odczytamy od uzytkownika i zapiszemy do zmiennej imie - zapisz nieznajomy jesli uztykownik nie udzieli odpowiedzi
+name = Console.ReadLine() ?? "Nieznajomy";
+string greeting = "Witaj " + name + "!";
+//wpisz gotowe powitanie
+Console.WriteLine(greeting);
